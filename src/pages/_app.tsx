7f85999f6b3fa5871e9ce6a91d7f7f47ba3b5280
EGOIST <0x142857@gmail.com>
2020-12-31 19:14:39 +0800

    use bar-of-progress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,22 @@
 import '../css/tailwind.css'
-import '../css/nprogress.css'
 import '../css/main.css'
 import '../css/prose.css'
 import React from 'react'
 import { useUrqlClient } from '@src/lib/urql-client'
 import { Provider as UrqlProvider } from 'urql'
-import nprogress from 'nprogress'
 import Router from 'next/router'
 
-nprogress.configure({
-  showSpinner: false,
-})
+if (process.browser) {
+  const ProgressBar: typeof import('@badrap/bar-of-progress') = require('@badrap/bar-of-progress')
 
-Router.events.on('routeChangeStart', (url) => {
-  nprogress.start()
-})
-Router.events.on('routeChangeComplete', () => nprogress.done())
-Router.events.on('routeChangeError', () => nprogress.done())
+  const progress = new ProgressBar.default()
+
+  Router.events.on('routeChangeStart', () => {
+    progress.start()
+  })
+  Router.events.on('routeChangeComplete', () => progress.finish())
+  Router.events.on('routeChangeError', () => progress.finish())
+}
 
 function MyApp({ Component, pageProps }: any) {
   const urqlClient = useUrqlClient()
