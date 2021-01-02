5003525effb1524f22c943ba268dec0517c98e97
EGOIST <0x142857@gmail.com>
2021-01-02 16:34:40 +0800

    add animation to refresh button

--- src/components/LeftPanel.tsx
+++ src/components/LeftPanel.tsx
@@ -144,7 +144,13 @@ export const LeftPanel = () => {
             }}
             className="h-full w-12 inline-flex justify-center items-center hover:bg-gray-100 focus:outline-none"
           >
-            <svg focusable="false" width="1em" height="1em" viewBox="0 0 24 24">
+            <svg
+              focusable="false"
+              width="1em"
+              height="1em"
+              viewBox="0 0 24 24"
+              className={clsx(topicsQuery.stale && `animate-spin`)}
+            >
               <g fill="none">
                 <path
                   d="M4 4v5h.582m15.356 2A8.001 8.001 0 0 0 4.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 0 1-15.357-2m15.357 2H15"
