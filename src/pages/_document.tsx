39b4fdf9165dba29e1650ab730f1bd8836d63bde
EGOIST <0x142857@gmail.com>
2021-03-29 22:39:02 +0800

    add cloudflare analytics

---
+++ src/pages/_document.tsx
@@ -0,0 +1,27 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: any) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+          <script
+            defer
+            src="https://static.cloudflareinsights.com/beacon.min.js"
+            data-cf-beacon='{"token": "4514c386de584b889d8b465387e46079"}'
+          ></script>
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
