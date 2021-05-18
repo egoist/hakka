c67fa0ac3686f909974ce234e3caf459554c5943
EGOIST <0x142857@gmail.com>
2021-05-18 12:53:15 +0800

    use volument

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,11 +19,18 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
           {process.env.NODE_ENV === 'production' && (
-            <script
-              defer
-              src="https://static.cloudflareinsights.com/beacon.min.js"
-              data-cf-beacon='{"token": "4514c386de584b889d8b465387e46079"}'
-            ></script>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: `
+           <script
+           defer
+           src="https://static.cloudflareinsights.com/beacon.min.js"
+           data-cf-beacon='{"token": "4514c386de584b889d8b465387e46079"}'
+         ></script>
+         <script async src="https://cdn.volument.com/v1/volument.js" onload="volument('8c101fdf29')"></script>
+           `,
+              }}
+            ></div>
           )}
         </body>
       </Html>
