5a9d2c0eca138068baf7a82b23359653f4b33fbb
EGOIST <0x142857@gmail.com>
2021-10-25 15:39:15 +0800

    use umami

--- next.config.js
+++ next.config.js
@@ -13,14 +13,6 @@ module.exports = {
         source: '/feed.json',
         destination: '/api/feed',
       },
-      {
-        source: '/bee.js',
-        destination: 'https://cdn.splitbee.io/sb.js',
-      },
-      {
-        source: '/_hive/:slug',
-        destination: 'https://hive.splitbee.io/:slug',
-      },
     ]
   },
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,7 +19,12 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
           {process.env.NODE_ENV === 'production' && (
-            <script async data-api="/_hive" src="/bee.js"></script>
+            <script
+              async
+              defer
+              data-website-id="8d61da99-f2f9-4820-ab55-3d674db3f1fc"
+              src="https://umami.egoist.sh/umami.js"
+            ></script>
           )}
         </body>
       </Html>
